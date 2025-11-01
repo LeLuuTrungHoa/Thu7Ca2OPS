ca007d52769c012aff2b643a04027a451616275f
Quoc Huy <darkness1652004@gmail.com>
2025-11-01 11:18:13 +0700

    HamKiemTraSoNguyenToVaGoiTrongMain

--- Source.cpp
+++ Source.cpp
@@ -9,12 +9,30 @@ int hieu(int a, int b){
 }
 int tich(int a, int b);
 float thuong(int a, int b);
+bool kiemTraSNT(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    int limit = sqrt(n);
+    for (int i = 2; i <= limit; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Ví dụ a là 2, b là 1";
 	cout<<"Tong =" << tong(2,1);
-	cout<<"Hieu =" << hieu(2,1);;
+	cout<<"Hieu =" << hieu(2,1);
+	cout<<"Kiem tra so 7 co phai so nguyen to";
+	if(kiemTraSNT(7)){
+		cout<<"7 la so nguyen to";
+	}
+	else
+		cout<<"7 khong la so nguyen to";
 	system("pause");
 	return 0;
 }
