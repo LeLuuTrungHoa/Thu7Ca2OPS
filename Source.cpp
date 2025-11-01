34b25888f5b347f0e6c322c5c15f16b7d532856a
LeLuuTrungHoa <hoatrung33@gmail.com>
2025-11-01 11:18:26 +0700

    them chuc nang ktr so chinh phuong

--- Source.cpp
+++ Source.cpp
@@ -9,12 +9,20 @@ int hieu(int a, int b){
 }
 int tich(int a, int b);
 float thuong(int a, int b);
+bool ktrsochinhphuong(int n) {
+    if (n < 0) {
+        return false;
+    }
+    double squareRoot = sqrt(n);
+    return (squareRoot == floor(squareRoot));
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Ví dụ a là 2, b là 1";
 	cout<<"Tong =" << tong(2,1);
 	cout<<"Hieu =" << hieu(2,1);;
+	cout<<"ChinhPhuong =" << ktrsochinhphuong(9);
 	system("pause");
 	return 0;
 }
