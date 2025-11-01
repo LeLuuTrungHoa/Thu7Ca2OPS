962767aeb53ed17fcd4c3e3d9235647f008deedc
BaoCameo <bao6pgg@gmail.com>
2025-11-01 11:20:43 +0700

    Bao

--- Source.cpp
+++ Source.cpp
@@ -12,7 +12,14 @@ int hieu(int a, int b){
 int tich(int a, int b){
 	return a*b;
 }
-float thuong(int a, int b);
+float thuong(int a, int b){
+	if (divisor == 0) {
+        coutr << "Lỗi: Không thể chia cho 0." << endl;   
+        return 0; 
+    }
+    return a / b;
+}
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
